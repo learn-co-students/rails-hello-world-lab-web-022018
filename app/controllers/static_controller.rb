781b7fabbfffccf8c1857b62269c0ea09a4f0114
github username <github email address>
2018-03-16 09:24:38 -0400

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,11 @@
+class StaticController < ApplicationController
+  def about
+    "some_page"
+  end
+end
+
+class StaticController < ApplicationController
+  def about
+    "hello_world"
+  end
+end
